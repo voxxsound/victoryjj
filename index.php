7224a30bda6c43d97a6253113858fd598b51adba
Voxxsound - Tower <voxxsound@gmail.com>
2013-03-09 16:13:30 -0500

    changed to php

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title> MMA Albany, NY | Victory Brazilian Jiu Jitsu.</title>
+		<meta name="verify-v1" content="Lf0REbILZSFq0KWVBt6VK0RiU4/dzIHiE4yB1QiHFLE=" />
+		<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+		<meta name="description" content="Located in Albany NY. We Specialize in Brazilian Jiu Jitsu/MMA, which includes grappling and superior ground fighting.  Call or stop by to schedule a free introductory class today!" />
+		<meta name="keywords" content=" Albany MMA, MMA Albany NY " />
+		<link href="victory.css" rel="stylesheet" type="text/css">
+	</head>
+	<body>
+		<div id=header>
+			<?php
+				echo '<p>This is the header</p>';
+			?>
+		<div>
+		<div id=body>
+			This is the first line of the body.
+			And I am adding a new line
+		</div>
+		<div id=footer>
+			<?php
+				echo '<p>This is the footer</p>';
+			?>
+		</div>
+	</body>
+</html>
