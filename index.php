0fc797cc32bad158e252bdc6024a2b6576d830f0
Voxxsound - Tower <voxxsound@gmail.com>
2013-06-03 21:32:25 -0400

    cleanup and final setup

--- index.php
+++ index.php
@@ -6,22 +6,25 @@
 		<meta http-equiv="content-type" content="text/html; charset=UTF-8">
 		<meta name="description" content="Located in Albany NY. We Specialize in Brazilian Jiu Jitsu/MMA, which includes grappling and superior ground fighting.  Call or stop by to schedule a free introductory class today!" />
 		<meta name="keywords" content=" Albany MMA, MMA Albany NY " />
-		<link href="victory.css" rel="stylesheet" type="text/css">
+		<link href="stylesheets/screen.css" media="screen, projection" rel="stylesheet" type="text/css" />
+		<link href="/stylesheets/print.css" media="print" rel="stylesheet" type="text/css" />
+		<!--[if IE]>
+		  <link href="/stylesheets/ie.css" media="screen, projection" rel="stylesheet" type="text/css" />
+		<![endif]-->
 	</head>
 	<body>
 		<div id=header>
 			<?php
-				echo '<p>This is the header</p>';
-			?>
-		<div>
+				echo <p>This is the header</p>
+		</div>
 		<div id=body>
 			This is the first line of the body.
 			And I am adding a new line
 		</div>
 		<div id=footer>
+			<p>This is the footer..</p>
 			<?php
-				echo '<p>This is the footer</p>';
-			?>
+				echo <p>This is text from within PHP tags in the footer</p>
 		</div>
 	</body>
 </html>
